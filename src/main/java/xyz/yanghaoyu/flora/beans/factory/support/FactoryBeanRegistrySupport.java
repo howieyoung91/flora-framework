20ae80ec4a69411aba14d90d1701a798fc1c3220
Howie <ishowieyoung@gmail.com>
2021-08-11 17:47:30 +0800

    实现FactoryBean

---
+++ src/main/java/xyz/yanghaoyu/flora/beans/factory/support/FactoryBeanRegistrySupport.java
@@ -0,0 +1,10 @@
+package xyz.yanghaoyu.flora.beans.factory.support;
+
+/**
+ * @author <a href="https://www.yanghaoyu.xyz">Howie Young</a><i>on 2021/8/11 11:12<i/>
+ * @version 1.0
+ */
+
+
+public class FactoryBeanRegistrySupport {
+}
---
+++ src/test/java/xyz/yanghaoyu/flora/test/IUserDao.java
@@ -0,0 +1,10 @@
+package xyz.yanghaoyu.flora.test;
+
+/**
+ * @author <a href="https://www.yanghaoyu.xyz">Howie Young</a><i>on 2021/8/11 11:28<i/>
+ * @version 1.0
+ */
+
+
+public interface IUserDao {
+}
