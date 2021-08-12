eae66eda2702a8b950295dd4cc5f71b652d4c92f
Howie <ishowieyoung@gmail.com>
2021-08-12 11:46:17 +0800

    实现了ApplicationEvent

---
+++ src/main/java/xyz/yanghaoyu/flora/context/ApplicationListener.java
@@ -0,0 +1,10 @@
+package xyz.yanghaoyu.flora.context;
+
+/**
+ * @author <a href="https://www.yanghaoyu.xyz">Howie Young</a><i>on 2021/8/11 20:43<i/>
+ * @version 1.0
+ */
+
+
+public class ApplicationListener {
+}
---
+++ src/main/java/xyz/yanghaoyu/flora/context/event/SimpleApplicationEventMulticaster.java
@@ -0,0 +1,10 @@
+package xyz.yanghaoyu.flora.context.event;
+
+/**
+ * @author <a href="https://www.yanghaoyu.xyz">Howie Young</a><i>on 2021/8/11 20:47<i/>
+ * @version 1.0
+ */
+
+
+public class SimpleApplicationEventMulticasterImpl {
+}
