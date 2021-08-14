9d5f3cbae38467f0f8d7be2d5889b512da1df209
Howie <ishowieyoung@gmail.com>
2021-08-14 23:37:36 +0800

    基本实现aop

---
+++ src/main/java/xyz/yanghaoyu/flora/constant/XmlTagConst.java
@@ -0,0 +1,25 @@
+package xyz.yanghaoyu.flora.constant;
+
+/**
+ * @author <a href="https://www.yanghaoyu.xyz">Howie Young</a><i>on 2021/8/14 21:43<i/>
+ * @version 1.0
+ */
+
+
+public interface XmlTagName {
+    String BEANS = "beans";
+    String BEAN = "bean";
+    String PROPERTY = "property";
+    String COMPONENT_SCAN = "component-scan";
+    String BASE_PATH = "base-path";
+    String BASE_PACKAGE = "base-package";
+    String INIT_METHOD = "init-method";
+    String DESTROY_METHOD = "destroy-method";
+    String CLASS = "class";
+    String ID = "id";
+    String SCOPE = "scope";
+    String REF = "ref";
+    String VALUE = "value";
+    String NAME = "name";
+    String ENABLE_PROPERTY_PLACEHOLDER_CONFIGURER = "enable-property-placeholder-configurer";
+}
