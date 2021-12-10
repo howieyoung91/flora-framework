e2cb2ea830bf6aacd73891518bb1bf340812856a
Howie <ishowieyoung@gmail.com>
2021-12-10 22:30:48 +0800

    删除了test包

--- src/test/java/xyz/yanghaoyu/flora/test/asm/AccountGeneratorClassLoader.java
+++
@@ -1,7 +0,0 @@
-package xyz.yanghaoyu.flora.test.asm;
-
-class AccountGeneratorClassLoader extends ClassLoader {
-    public Class defineClassFromClassFile(String className, byte[] classFile) throws ClassFormatError {
-        return defineClass(className, classFile, 0, classFile.length);
-    }
-}
